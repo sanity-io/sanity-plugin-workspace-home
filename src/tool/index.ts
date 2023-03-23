721e2812e9f02911cc4e4869e6420d4e129ec059
spacedawwwg <spacedawwwg@gmail.com>
2023-03-23 15:37:33 +0000

    feat: make tool title configurable

--- src/tool/index.ts
+++ src/tool/index.ts
@@ -1,10 +1,11 @@
 import {HomeIcon} from '@sanity/icons'
+import type {Tool} from 'sanity'
 
 import WorkspaceHome from '../components/WorkspaceHome'
 
-export const workspaceHomeTool = {
-  title: 'Workspace Home',
+export const workspaceHomeTool = (title: string): Tool => ({
+  title,
   name: 'workspace-home',
   icon: HomeIcon,
   component: WorkspaceHome,
-}
+})
